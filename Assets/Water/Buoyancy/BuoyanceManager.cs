c93fc18f8764452ed760faacb28db3f4ae012d81
Jung Jin Sun <realline@kpu.ac.kr>
2021-04-11 02:24:13 +0900

    부력 normal vector 계산 수정
    new normal이 -값 나오던 부분 수정

--- Assets/Water/Buoyancy/BuoyanceManager.cs
+++ Assets/Water/Buoyancy/BuoyanceManager.cs
@@ -30,6 +30,7 @@ public class BuoyanceManager : MonoBehaviour
     {
         _guid = gameObject.GetInstanceID();
 
+        // 자식으로 있는 부력 오브젝트 가져오기
         buoyantObjs = new Transform[transform.childCount];
         points = new NativeArray<float3>(buoyantObjs.Length, Allocator.Persistent);
         heights = new float3[buoyantObjs.Length];
@@ -38,36 +39,41 @@ public class BuoyanceManager : MonoBehaviour
         for (var i = 0; i < buoyantObjs.Length; i++)
         {
             buoyantObjs[i] = transform.GetChild(i);
-            Debug.Log(buoyantObjs[i].ToString());
+            Debug.Log(buoyantObjs[i].name);
             points[i] = buoyantObjs[i].position;
         }
 
-        // 수정 필요
+        // wave 정보 가져오기
         _waveCount = 4; 
         _waveData = new NativeArray<Wave>(_waveCount, Allocator.Persistent);
         
         Wave wave = new Wave();
 
         Material material = GetComponent<Renderer>().material;
-        
+        var time = material.GetVector("Vector4_2DF06505");
+
         wave.amplitude = material.GetFloat("Vector1_7EAA09FD");
+        wave.time = time.x;
         wave.direction = new float3(material.GetVector("Vector3_BB5DA054").x, material.GetVector("Vector3_BB5DA054").y, material.GetVector("Vector3_BB5DA054").z); ;
         _waveData[0] = wave;
-        Debug.Log("wave.amplitude - " + Shader.GetGlobalFloat("Vector1_7EAA09FD"));
+        //Debug.Log("wave.amplitude - " + Shader.GetGlobalFloat("Vector1_7EAA09FD"));
 
         wave.amplitude = material.GetFloat("Vector1_C9F53F9");
+        wave.time = time.y;
         wave.direction = new float3(material.GetVector("Vector3_110DB033").x, material.GetVector("Vector3_110DB033").y, material.GetVector("Vector3_110DB033").z); ;
         _waveData[1] = wave;
 
         wave.amplitude = material.GetFloat("Vector1_F0AF1EEC");
+        wave.time = time.z;
         wave.direction = new float3(material.GetVector("Vector3_8495EC90").x, material.GetVector("Vector3_8495EC90").y, material.GetVector("Vector3_8495EC90").z); ;
         _waveData[2] = wave;
 
         wave.amplitude = material.GetFloat("Vector1_76F34D1B");
+        wave.time = time.w;
         wave.direction = new float3(material.GetVector("Vector3_13778AA8").x, material.GetVector("Vector3_13778AA8").y, material.GetVector("Vector3_13778AA8").z); ;
         _waveData[3] = wave;
 
-
+        // job system init
         GerstnerWavesJobs.Init();
     }
 
@@ -86,10 +92,12 @@ public class BuoyanceManager : MonoBehaviour
             var vec = buoyantObjs[i].position;
             vec.y = heights[i].y;
             buoyantObjs[i].position = vec;
-            Debug.Log(vec);
-            Debug.Log(normals[i]);
+            //Debug.Log(vec);
+            //Debug.Log(normals[i]);
 
             buoyantObjs[i].up = Vector3.Slerp(buoyantObjs[i].up, normals[i], Time.deltaTime);
+            //Debug.Log(buoyantObjs[i].up);
+            Debug.DrawRay(buoyantObjs[i].position, buoyantObjs[i].up * 10);
         }
     }
 
--- Assets/Water/Buoyancy/GerstnerWaveJobs.cs
+++ Assets/Water/Buoyancy/GerstnerWaveJobs.cs
@@ -9,12 +9,16 @@ using Unity.Collections;
 public struct Wave
 {
     public float amplitude;
+    public float time;
+
     public float3 direction;
 
-    public Wave(float amp, float3 dir)
+    public Wave(float amp, float t, float3 dir)
     {
         amplitude = amp;
+        time = t;
         direction = dir;
+        
     }
 }
 
@@ -39,9 +43,9 @@ public static class GerstnerWavesJobs
         _waveCount = BuoyanceManager.Instance._waveCount;
         _waveData = BuoyanceManager.Instance._waveData;
 
-        _positions = new NativeArray<float3>(4096, Allocator.Persistent);
-        _wavePos = new NativeArray<float3>(4096, Allocator.Persistent);
-        _waveNormal = new NativeArray<float3>(4096, Allocator.Persistent);
+        _positions = new NativeArray<float3>(16900, Allocator.Persistent);
+        _wavePos = new NativeArray<float3>(16900, Allocator.Persistent);
+        _waveNormal = new NativeArray<float3>(16900, Allocator.Persistent);
 
         Initialized = true;
     }
@@ -61,14 +65,17 @@ public static class GerstnerWavesJobs
 
     public static void UpdateSamplePoints(ref NativeArray<float3> samplePoints, int guid)
     {
+        // job이 돌아가고 있으면 complete
         CompleteJobs();
 
+        // 값이 있으면 값 전달
         if (Registry.TryGetValue(guid, out var offsets))
         {
             for (var i = offsets.x; i < offsets.y; i++) _positions[i] = samplePoints[i - offsets.x];
         }
         else
         {
+            // 정해놓은 vertex개수보다 많으면 return
             if (_positionCount/*position이 시작되는 index*/ + samplePoints.Length/*x,y,z,로 3*/ >= _positions.Length) return;
 
             offsets = new int2(_positionCount, _positionCount + samplePoints.Length);
@@ -79,6 +86,7 @@ public static class GerstnerWavesJobs
 
     public static void GetData(int guid, ref float3[] outPos, ref float3[] outNorm)
     {
+        // 값이 없으면 return
         if (!Registry.TryGetValue(guid, out var offsets)) return;
 
         // x, y, z인 값 3개를 잘라서 copy
@@ -90,6 +98,7 @@ public static class GerstnerWavesJobs
     // 다음 프레임 계산
     public static void UpdateHeights()
     {
+        // 이미 프로세싱 중이면 return
         if (_processing) return;
 
         _processing = true;
@@ -105,7 +114,6 @@ public static class GerstnerWavesJobs
         };
 
         _waterHeightHandle = waterHeight.Schedule(_positionCount, 32);
-
         JobHandle.ScheduleBatchedJobs();
 
         _firstFrame = false;
@@ -113,9 +121,13 @@ public static class GerstnerWavesJobs
 
     private static void CompleteJobs()
     {
+        // 첫 프레임이거나 프로세싱중이 아니면 return
         if (_firstFrame || !_processing) return;
 
+        // job complete
         _waterHeightHandle.Complete();
+
+        // 프로세싱 끝
         _processing = false;
     }
 
@@ -142,58 +154,56 @@ public static class GerstnerWavesJobs
         {
             if (i < OffsetLength.x || i >= OffsetLength.y - OffsetLength.x) return;
 
-            var waveCountMulti = 1f / WaveData.Length;
-            var wavePos = new float3(0f, 0f, 0f);
-            var waveNorm = new float3(0f, 0f, 0f);
+            //var waveCountMulti = 1f / WaveData.Length;
+            var wavePos = Position[i];
+            var waveNorm1 = new float3(Position[i].x +1f, Position[i].y, Position[i].z);
+            var waveNorm2 = new float3(Position[i].x, Position[i].y, Position[i].z + 1f);
+
+            var pos = Position[i].xz;
+            var normPos1 = new float2(Position[i].x + 1f, Position[i].z);
+            var normPos2 = new float2(Position[i].x, Position[i].z + 1f);
+
 
             for (var wave = 0; wave < WaveData.Length; wave++) // for each wave
             {
-
-                var pos = Position[i].xz;
-                float3 vec1 = new float3(0f, 0f, 0f), vec2 = new float3(0f, 0f, 0f);
-
                 // wave
-                wavePos += calcGerstner(wavePos, wave, Position[i].xz);
-                Debug.Log("wavePos - " + wavePos);
+                calcGerstner(ref wavePos, wave, pos);
 
                 // new normal
-                vec1 = calcGerstner(vec1, wave, new float2(Position[i].x + 0.1f, Position[i].z));
-                vec2 = calcGerstner(vec2, wave, new float2(Position[i].x, Position[i].z + 0.1f));
-                Debug.Log("vec1 - " + vec1);
-                Debug.Log("vec2 - " + vec2);
-
-                waveNorm += math.cross(vec1, vec2);
+                calcGerstner(ref waveNorm1, wave, normPos1);
+                calcGerstner(ref waveNorm2, wave, normPos2);
             }
 
             OutPosition[i] = wavePos;
-            OutNormal[i] = math.normalize(waveNorm);
-            Debug.Log("outNormal - " + OutNormal[i]);
+            var normal = math.normalize(math.cross(math.normalize(waveNorm2 - wavePos), math.normalize(waveNorm1 - wavePos)));
+            OutNormal[i] = normal;
+
+            //Debug.Log("outNormal - " + math.normalize(math.cross(math.normalize(waveNorm2 - wavePos), math.normalize(waveNorm1 - wavePos))));
         }
 
-        private float3 calcGerstner(float3 wavePos, int wave, float2 pos)
+        private void calcGerstner(ref float3 wavePos, int wave, float2 pos)
         {
             var amplitude = WaveData[wave].amplitude;
             var direction = WaveData[wave].direction;
 
-            Debug.Log("WaveData[wave].amplitude - " + WaveData[wave].amplitude);
-            Debug.Log("WaveData[wave].direction - " + WaveData[wave].direction);
+            //Debug.Log("WaveData[wave].amplitude - " + WaveData[wave].amplitude);
+            //Debug.Log("WaveData[wave].direction - " + WaveData[wave].direction);
 
             float depth = 10f;
             float length = math.length(direction);
-            Debug.Log("length - " + length);
+            //Debug.Log("length - " + length);
 
             float tanh = math.tanh(length * depth);
-            Debug.Log("tanh - " + tanh);
+            //Debug.Log("tanh - " + tanh);
 
-            float frequency = math.sqrt(length * 9.8f * tanh);
-            Debug.Log("frequency - " + frequency);
+            float frequency = math.trunc(math.sqrt(length * 9f * tanh) * 1000) / 1000; 
+            //Debug.Log("frequency - " + frequency);
             float phase = 0;
-            float theta = (direction.x * pos.x) + (direction.z * pos.y/*z*/) - (frequency * Time) - phase;
+            float theta = math.trunc(((direction.x * pos.x) + (direction.z * pos.y/*z*/) - (frequency * Time * WaveData[wave].time) - phase) * 1000) / 1000;
 
-            wavePos.x += -((direction.x / length) * (amplitude / tanh) * math.sin(theta));
-            wavePos.y += amplitude * math.cos(theta);
-            wavePos.z += -((amplitude / tanh) * math.sin(theta));
-            return wavePos;
+            wavePos.x += -((direction.x / length) * (amplitude / tanh) * math.sin(theta)) ; 
+            wavePos.y += amplitude * math.cos(theta) ;
+            wavePos.z += -((direction.z / length) * (amplitude / tanh) * math.sin(theta));
         }
     }
 }
